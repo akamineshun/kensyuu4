c816ba2f77fa09e1903a1922873fdc630138c934
220291p <220291p@ugs.kochi-tech.ac.jp>
2025-04-08 15:22:27 +0900

    STEP７】Spring Boot + Thymeleafで新規登録画面を作成する（INSERT）途中まで

--- trainingspring-3/src/main/java/com/example/demo/service/UserService.java
+++ trainingspring-3/src/main/java/com/example/demo/service/UserService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
@@ -35,5 +37,20 @@ public class UserService {
 	 */
 	public User findById(Long id) {
 		return userRepository.findById(id).get();
+	}
+	
+	/**
+	 * ユーザー情報登録
+	 * @param user ユーザー情報
+	 */
+	public void create(UserRequest userRequest) {
+		Date now = new Date();
+		User user = new User();
+		user.setName(userRequest.getName());
+		user.setAddress(userRequest.getAddress());
+		user.setPhone(userRequest.getPhone());
+		user.setCreateDate(now);
+		user.setUpdateDate(now);
+		userRepository.save(user);
 	}
 }
